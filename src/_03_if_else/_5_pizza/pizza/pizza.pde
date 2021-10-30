13f4664c53c03df4952948e1edf80249dc7fc1b3
student <student@ip-172-31-54-90.us-west-2.compute.internal>
2021-10-30 12:56:31 -0700

    Riddle

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,19 +1,32 @@
 PImage pepperoni;
 PImage olive;
 void setup() {
-pepperoni=loadImage("pepperoni.png");
-olive=loadImage("olive.png");
-size(500,500);
-fill(#B1B256);
-ellipse(250,250,400,400);
-fill(#F20000);
-ellipse(250,250,300,300);
-fill(#DED15E);
-ellipse(250,250,250,250);
-pepperoni.resize(30,30);
-olive.resize(30,30);
+  pepperoni=loadImage("pepperoni.png");
+  olive=loadImage("olive.png");
+  size(500, 500);
+  fill(#B1B256);
+  ellipse(250, 250, 400, 400);
+  fill(#F20000);
+  ellipse(250, 250, 300, 300);
+  fill(#DED15E);
+  ellipse(250, 250, 250, 250);
+  pepperoni.resize(30, 30);
+  olive.resize(30, 30);
 }
 void draw() {
-    image(pepperoni,200,200);
-    image(olive,150,150);
+  image(pepperoni, 200, 200);
+  image(olive, 150, 150);
+  if (mousePressed&&mouseButton==LEFT) {
+    image(pepperoni, mouseX,mouseY);
+  }
+  if (mousePressed&&mouseButton==RIGHT) {
+    image(olive, mouseX,mouseY);
+  }
 }
+/*
+mousePressed (A)| mouseButton = LEFT (B) | A && B (C) | A || B (D) | !A (E)  |  A && (B || !C)   |  !C    | B || !C  | 
+true            | true                   | true       | true       | false   | true!             | false  | true     |
+true            | false                  | false      | true       | false   | false?True        | true   | true     |
+false           | true                   | false      | true       | true    | false!            | true   | true     |
+false           | false                  | false      | false      | true    | true?False        | true   | true     |
+*/
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,25 +1,46 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		String input = JOptionPane.showInputDialog("What has to be broken before you can use it?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (input.equals("an egg")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "correct");
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+		}
 		// 6. Add some more riddles
+		input = JOptionPane.showInputDialog("I’m tall when I’m young, and I’m short when I’m old. What am I?");
+		if (input.equals("a candle")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null,"correct");
+		} else {
 
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
+		input=JOptionPane.showInputDialog("What is full of holes but still holds water?");
+		if (input.equals("a sponge")) {
+			score +=1;
+			JOptionPane.showMessageDialog(null,"correct");		
+			}
+		else {
+			JOptionPane.showMessageDialog(null,"wrong");
+		}
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, "You got "+ score+" out of 3");
 	}
 }
-
